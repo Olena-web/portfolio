7ca139869e82505fdbb5d349e12ed9d1d4276ee6
Olena Honcharuk <gasmin07@gmail.com>
2021-08-11 01:28:33 +0300

    responsive layout

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,17 @@
-const hamburger = document.querySelector(".hamburger"),
-  menu = document.querySelector(".menu"),
-  closeElem = document.querySelector(".menu__close");
-hamburger.addEventListener("click", () => {
-  menu.classList.add("active");
-});
-closeElem.addEventListener("click", () => {
-  menu.classList.remove("active");
-});
-const counters = document.querySelectorAll(".progress__item-text"),
-  lines = document.querySelectorAll(".progress__item-indicator");
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger"),
+    menu = document.querySelector(".menu"),
+    closeElem = document.querySelector(".menu__close");
+  hamburger.addEventListener("click", () => {
+    menu.classList.add("active");
+  });
+  closeElem.addEventListener("click", () => {
+    menu.classList.remove("active");
+  });
+  const counters = document.querySelectorAll(".progress__item-text"),
+    lines = document.querySelectorAll(".progress__item-indicator");
 
-counters.forEach((item, i) => {
-  lines[i].style.width = item.innerHTML;
+  counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+  });
 });
