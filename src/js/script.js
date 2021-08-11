edebf28cbe0dcf40ed8f96c91bbf486c3e9337aa
Olena Honcharuk <gasmin07@gmail.com>
2021-08-11 22:40:24 +0300

    validation form, post

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,51 @@ document.addEventListener("DOMContentLoaded", () => {
     lines[i].style.width = item.innerHTML;
   });
 });
+$(document).ready(function () {
+  //validation
+
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 3,
+        },
+
+        email: {
+          required: true,
+          email: true,
+        },
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста, введите свое имя",
+          minlength: jQuery.validator.format(
+            "Как минимум {0} буквы требуется!"
+          ),
+        },
+
+        email: {
+          required: "Пожалуйста, введите свою почту",
+          email: "Неправильно введен адрес почты",
+        },
+      },
+    });
+  }
+  validateForms("#name");
+  validateForms("#email");
+
+  $("contacts__form").submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize(),
+    }).done(function () {
+      $(this).find("input").val("");
+
+      $("form").trigger("reset");
+    });
+    return false;
+  });
+});
